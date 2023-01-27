a6f40eae96f9cf1b05ad5ac610918b0f5add52ef
一车四木 <467914885@qq.com>
2023-01-27 19:51:08 +0800

    清单列表页面修改

---
+++ app/src/main/java/com/maureen/schedule/view/SwipeRecyclerView.kt
@@ -0,0 +1,16 @@
+package com.maureen.schedule.view
+
+import android.app.Notification.Action
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Description:
+ * @author Maureen
+ * Create 2023-01-25 14:03
+ */
+class SwipeRecyclerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null):RecyclerView(context, attrs) {
+
+}
